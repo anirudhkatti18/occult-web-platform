e1813ae9ab8df47ad4e45576293ae27af33d4647
Anirudh Katti <anirudhkatti18@gmail.com>
2026-03-14 04:55:03 +0530

    feat: Configure Next.js for static export, base path, asset prefix, and unoptimized images.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/occult-web-platform',
+  assetPrefix: '/occult-web-platform',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
