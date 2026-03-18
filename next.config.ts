7a6f69f993210089834c1cd78efa52b4b02f70ac
Anirudh Katti <anirudhkatti18@gmail.com>
2026-03-18 22:02:18 +0530

    Removed sub-directory routing for root domain

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/occult-web-platform',
-  assetPrefix: '/occult-web-platform',
   images: {
     unoptimized: true,
   },
