cecb4b49b27970dacbf7db19927b4e349a6918fd
Anirudh Katti <anirudhkatti18@gmail.com>
2026-03-21 23:24:53 +0530

    feat: add sitemap.ts to define site structure for search engines

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://occult.marketing',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://occult.marketing/about',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://occult.marketing/services',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://occult.marketing/contact',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ];
+}
